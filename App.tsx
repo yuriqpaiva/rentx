d3e6109097c7005332d94a3bd426407e1ad34bcc
Yuri <yuriqpaiva@gmail.com>
2022-08-26 16:14:22 -0300

    Add styled components theme configurations

--- App.tsx
+++ App.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useCallback, useEffect } from 'react';
 import { Home } from './src/screens/Home';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
 
-export default function App(): JSX.Element {
-  return <Home />;
+export default function App(): JSX.Element | null {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  useEffect(() => {
+    async function prepare(): Promise<void> {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <Home />
+      </View>
+    </ThemeProvider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
