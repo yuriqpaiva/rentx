025b8423b931452cbdde00eb4a0af4003b65cc98
Yuri <yuriqpaiva@gmail.com>
2022-08-26 18:14:14 -0300

    Add car card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,31 @@ import { StatusBar } from 'react-native';
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 import Logo from '../../assets/logo.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { Car } from '../../components/Car';
 
 export function Home(): JSX.Element {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail:
+      'https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png',
+  };
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail:
+      'https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_low/v1/editorial/vhs/Porsche-Panamera.png',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -18,6 +41,9 @@ export function Home(): JSX.Element {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
