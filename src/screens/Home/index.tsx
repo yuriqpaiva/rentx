cc5bbcc4ba1721d5c79ae8cc2ab03bad51e86a19
Yuri <yuriqpaiva@gmail.com>
2022-08-26 16:38:56 -0300

    Add app header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { StatusBar } from 'react-native';
+import { Container, Header, TotalCars, HeaderContent } from './styles';
+import Logo from '../../assets/logo.svg';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export function Home(): JSX.Element {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
