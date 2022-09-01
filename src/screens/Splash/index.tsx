245e74ed7f564738a223d4b77d69afc2b0e3f2e2
Yuri <yuriqpaiva@gmail.com>
2022-08-31 21:11:56 -0300

    Add animation example with reanimated library

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
 import { Container } from './styles';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
 export function Splash(): JSX.Element {
-  return <Container></Container>;
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value * 100 }],
+    };
+  });
+
+  function handleAnimationPosition(): void {
+    animation.value = withSpring(Math.random());
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button title="Mover" onPress={handleAnimationPosition} />
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
