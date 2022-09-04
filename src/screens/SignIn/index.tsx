f41e2983b09d36e10231f51b0e4a83a9061de6c0
Yuri <yuriqpaiva@gmail.com>
2022-09-04 13:04:41 -0300

    Add sign in validation with yup

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,18 +4,45 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from 'react-native';
 import { useTheme } from 'styled-components';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
 import { Container, Header, Subtitle, Title, Form, Footer } from './styles';
+import * as Yup from 'yup';
 
 export function SignIn(): JSX.Element {
   const theme = useTheme();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  async function handleSignIn(): Promise<void> {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('A senha é obrigatória'),
+      });
+
+      await schema.validate({ email, password });
+      Alert.alert('Tudo certo!');
+
+      // Login
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert('Opa', error.message);
+      } else {
+        return Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer o login, verifique as credenciais',
+        );
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -55,13 +82,12 @@ export function SignIn(): JSX.Element {
           <Footer>
             <Button
               title="Login"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
             />
             <Button
               title="Criar conta gratuita"
-              onPress={() => {}}
               enabled={false}
               loading={false}
               color={theme.colors.background_secondary}
